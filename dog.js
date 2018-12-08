2ef5db1c8dbaa4934d6e62f7747c47d1c2c2bd10
truptigaonkar <trups11.trupti@gmail.com>
2018-12-08 19:55:20 +0100

    Updated HTML & CSS for styling

--- dog.js
+++ dog.js
@@ -1,6 +1,6 @@
 let mainBreedList = document.querySelector('#mainBreedList');
-// Getting list of all dog breeds
-function getBreeds(){
+////================Getting list of all dog breeds==================////
+function getBreeds() {
     let xml = new XMLHttpRequest
     xml.open("GET", "https://dog.ceo/api/breeds/list/all");
     xml.addEventListener("load", getBreedsParse);
@@ -9,52 +9,53 @@ function getBreeds(){
 getBreeds(); // Declare function
 
 //Parsing json data from breed list 'https://dog.ceo/api/breeds/list/all'
-function getBreedsParse(){
+function getBreedsParse() {
     let parseData = JSON.parse(this.responseText);
     console.log(parseData.message);
     renderText(parseData.message);
 }
 
 //Rendering HTML content in to list
-function renderText(getBreedsParseData){
+function renderText(getBreedsParseData) {
     let myUl = document.querySelector("#getBreeds");
-    for (let i in getBreedsParseData){
+    for (let i in getBreedsParseData) {
         let li = document.createElement("li");
         li.textContent = i;
         myUl.appendChild(li);
     }
 }
-////--------End of list of all dog breeds-----------------------------------////
+////================Emd of getting list of all dog breeds==================////
 
-//Getting the random image
-function getRandomImages(){
+////================Getting the random image==================////
+function getRandomImages() {
     let xml = new XMLHttpRequest
     xml.open("GET", "https://dog.ceo/api/breeds/image/random");
     xml.addEventListener("load", getRandomImagesParse);
     xml.send();
+    //Adding event listner to the button to get the images randomly
     let getBtn = document.querySelector('#getBtn');
     getBtn.addEventListener('click', getRandomImages);
 }
 getRandomImages(); // Declare function
 
 //Parsing json data from 'https://dog.ceo/api/breeds/image/random'
-function getRandomImagesParse(){
+function getRandomImagesParse() {
     let parseData = JSON.parse(this.responseText);
     console.log(parseData);
     renderImg(parseData.message);
 }
 
 //Rendering images on webpage
-function renderImg(imgData){
-    for (let key in imgData){
-        let checkImg = document.querySelector("img");
-        if (checkImg){
-            mainBreedList.removeChild(checkImg);
-        }
-        let img = document.createElement("img");
-        img.setAttribute("src", imgData);
-        mainBreedList.appendChild(img);
+function renderImg(imgData) {
+    // Avoiding to get multiple images
+    let checkImg = document.querySelector("img");
+    if (checkImg) {
+        mainBreedList.removeChild(checkImg);
     }
+    // Create image element
+    let img = document.createElement("img");
+    img.setAttribute("src", imgData);
+    mainBreedList.appendChild(img);
 }
-////--------End of listing random images-----------------------------------////
+////================End of getting the random image==================////
 
